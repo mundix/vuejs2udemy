02e6e66e216544418321a2de0eb8658d29ac4a0b
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-17 07:05:59 -0400

    Directivas v-bind and v-html and v-text :src ...

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const app = new Vue ({
     el:"#vm",
     data:{
-       
+    //    imagen: 'https://m.media-amazon.com/images/M/MV5BNGM5MTEyZDItZWNhOS00NzNkLTgwZTAtNWIzY2IzZmExOWMxXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg'
+        text: "<strong>Bienvenido</strong> al curso de Vue Js 2!"
     },
     
 });
