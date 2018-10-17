bf9ee27b772dfe9e81e6426a8a141366423b1cac
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-17 01:13:04 -0400

    finalizo eventos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const app = new Vue ({
     el:"#vm",
     data:{
-       
+       nombre: 'Edmundo'
     },
+    //Interpolaciones  v-on:keypup // escuchamos un evento  Enlace de Datos
+    // como hcaer un enlace de datos doble  v-model 
     
 });
